4dd91117ed4c3507ccdadc6de652dea9a5cb0302
Gabetn <gabriel.negash@mail.mcgill.ca>
2019-03-10 05:21:21 -0400

    Refactored code, ready to record

--- src/test/java/ecse428a2/StepDefinitions.java
+++ src/test/java/ecse428a2/StepDefinitions.java
@@ -1,12 +1,14 @@
 package ecse428a2;
 
 import cucumber.api.PendingException;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.*;
 import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.internal.WebElementToJsonConverter;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,21 +17,14 @@ import java.awt.datatransfer.*;
 import java.awt.event.KeyEvent; //Used for mocking user key strokes for windows file explorer
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class StepDefinitions {
 
-    // Variables
-    private final String PRODUCT_URL = "https://www.amazon.ca/Monoprice-115365-Select-Mini-Printer/dp/B01FL49VZE/ref=sr_1_1?ie=UTF8&qid=1488132110&sr=8-1&keywords=3d+printer";
-    private final String PRODUCT_NAME = "Monoprice 115365 Monoprice Select Mini 3D Printer";
-    private final String DELETE_BTN_NAME = "submit.delete.C3NLW69582M4B4";
-    private final String CART_URL = "https://www.amazon.ca/gp/cart/view.html/ref=nav_cart";
-    private final String ADD_TO_CART_BTN = "add-to-cart-button";
-    private final String ACTIVE_CART = "sc-active-cart";
-    private final String CHECKOUT_BTN = "sc-proceed-to-checkout";
-
     // MY VARIABLES
     private WebDriver driver;
+    private WebDriverWait waiter;
     private Alert alert;
     private final String PATH_TO_CHROME_DRIVER = "C:\\Users\\Gabriel\\Documents\\Applications\\chromedriver_win32\\chromedriver.exe";
     private final String LOG_IN = "https://accounts.google.com/AccountChooser?service=mail&continue=https://mail.google.com/mail/";
@@ -38,33 +33,29 @@ public class StepDefinitions {
     private final String CONFIRMATION = "Message sent.";
     private final String WARNING = "Send this message without a subject or text in the body?";
 
-    @Before
-    public void setUpTests(){
+    @Before //TODO
+    public void setUpTests(Scenario scenario){
         //I.e. if failed to login to sent tab
-        if(!setupTearDownHelper()){ Assert.fail(); }
-
-        //Verify number of emails sent = 0
-        Assert.assertTrue(driver.findElements(By.xpath("//div[contains(@aria-label,'Show more messages')]")).isEmpty());
-        //NOTE: assumes this div is only visible when an email in the inbox is present
-    }
-
-    @After
-    public void tearDownTests(){
-        //I.e. if failed to login to sent tab
-        if(!setupTearDownHelper()){ Assert.fail(); }
-
-        //DELETE any sent emails
-        if(!driver.findElements(By.xpath("//div[contains(@aria-label,'Show more messages')]")).isEmpty()){
-            //Click select all button
-            WebElement selector = driver.findElement(By.xpath("//span[contains(@class,'T-Jo J-J5-Ji')]"));
-            selector.click();
-
-            //Click delete button
-            WebElement delete = driver.findElement(By.xpath("//div[contains(@aria-label,'Delete')]"));
-            delete.click();
+        if(scenario.getName().equals("Attaching an image and sending to an invalid recipient")){
+            driver = null;
+            return;
         }
-    }
 
+        System.out.println("SetUp");
+        if(!setUpHelper()){
+            System.out.println("Failed to setUpHelper!"); //
+            Assert.fail(); }
+
+        goToSent();
+        //Verify number of emails sent = 0
+        WebElement message = waiter.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//td[contains(text(),'No sent messages!')]")));
+        //int count = driver.findElements(By.xpath("//div[contains(@aria-label,'Show more messages')]")).size();
+        System.out.println("Message is: \t"+message.getText());
+        Assert.assertTrue(message!=null);
+        //NOTE: assumes this div is only visible when an email in the inbox is present
+        driver.quit();
+        driver = null; //reset driver
+    }
 
     // Given
     @Given("^I am logged in$")
@@ -98,7 +89,7 @@ public class StepDefinitions {
     }
 
     // When
-    @When("^I click the ‘Compose’ button$")
+    @When("^I compose an email$")
     public void iPressCompose() throws Throwable {
         try {
             System.out.println("Attempting to find Compose button... ");
@@ -113,7 +104,7 @@ public class StepDefinitions {
         }
     }
 
-    @And("^I enter a valid ([^\"]*) in the ‘to’ section$")
+    @And("^I enter a valid ([^\"]*) recipient$")
     public void enterValidEmail(String validEmail) throws Throwable { //TODO: how to use scenario outline
         try {
             System.out.println("Attempting to find Recipients Field... ");
@@ -129,7 +120,7 @@ public class StepDefinitions {
             System.out.println("No Recipient field found");
         }
     }
-    @And("^I input an invalid ([^\"]*) in the ‘to’ section$")
+    @And("^I input an invalid ([^\"]*) recipient$")
     public void enterInvalidEmail(String invalidEmail) throws Throwable { //TODO: how to use scenario outline
         try {
             System.out.println("Attempting to find Recipients Field... ");
@@ -146,43 +137,26 @@ public class StepDefinitions {
         }
     }
 
-    @And("^I enter ([^\"@]*) in ([^\"@]*) section$")
-    public void enterContent(String text, String location) throws Throwable { //TODO: how to use scenario outline
+    @And("^I enter subject ([^\"@]*)$")
+    public void enterContent(String text) throws Throwable { //TODO: how to use scenario outline
         try {
-            WebElement textField = findElement(location);
-            System.out.println("Entering text...");
-            enterText(textField,text,Keys.TAB); //TODO replace text w/ outline value
-            System.out.println("Entered");
-            //Note: In case of both, first subject is done by default, then body
-            if (location.equals("both")){
-                textField = findElement("body");
-                System.out.println("Entering body...");
-                enterText(textField,text,Keys.ENTER);
-            }
+            System.out.println("Attempting to find subject Field");
+            WebElement textField = (new WebDriverWait(driver, 10))
+                    .until(ExpectedConditions.elementToBeClickable(
+                            By.xpath("//input[contains(@aria-label,'Subject')]"))); //NOTE assumes english
+            System.out.print("Found!\n");
+            enterText(textField, text, Keys.TAB); //TODO replace text w/ outline value
         } catch (Exception e) {
-            System.out.println("No Recipient field found");
-        }
+                System.out.println("No Recipient field found");
+            }
     }
 
-    // When
-    @And("^I click the ‘Attach files’ button$")
-    public void iPressAttach() throws Throwable {
-        try {
-            System.out.println("Attempting to find Attach button... ");
-            WebElement btn = (new WebDriverWait(driver, 10))
-                    .until(ExpectedConditions.elementToBeClickable(
-                            By.xpath("//div[contains(@class,'a1 aaA aMZ')]"))); //Note: assumes class name doesn't change
-            System.out.print("Found!\n");
-            btn.click();
-            Thread.sleep(1000); //TODO: deal with timing
-            System.out.println("Clicking Attach button.");
-        } catch (Exception e) {
-            System.out.println("No Attach button found");
-        }
-    }
+
 
     @When("^I select the file ([^\"]*) I want to send$")
     public void selectFile(String fPath) throws Throwable { //TODO: how to use scenario outline
+        iPressAttach();
+
         String[] names = fPath.split("\n");
         StringBuilder sb = new StringBuilder();
         for(String s : names){
@@ -208,7 +182,7 @@ public class StepDefinitions {
 
     }
 
-    @And("^I click the ‘Send’ button$")
+    @And("^I send the email$")
     public void iPressSend() throws Throwable {
         try {
             System.out.println("Attempting to find Send button... ");
@@ -216,7 +190,7 @@ public class StepDefinitions {
                     .until(ExpectedConditions.elementToBeClickable(
                             By.xpath("//div[contains(text(),'Send')]"))); //TODO assumes english
             System.out.print("Found!\n");
-            Thread.sleep(5000);
+            //Thread.sleep(5000);
             btn.click();
             System.out.println("Clicking Send button.");
         } catch (Exception e) {
@@ -230,16 +204,37 @@ public class StepDefinitions {
     public void isEmailSent() throws Throwable {
         System.out.println("Attempting to find Confirmation ...");
         //NOTE: Assumes span is only visible within html after email sent as opposed to simply being hidden
-        WebElement dialog = (new WebDriverWait(driver, 15))
+        WebElement dialog = (new WebDriverWait(driver, 20))
                 .until(ExpectedConditions.elementToBeClickable(
                         By.xpath("//span[contains(text(),'Message sent.')]"))); //NOTE assumes english
         System.out.print("Found!\n");
         System.out.println("Dialog Text: "+dialog.getText());
         Assert.assertEquals(dialog.getText(),CONFIRMATION); //NOTE: assumes confirmation message doesn't change
         //Thread.sleep(5000); //For user visibility purposes during execution
-        driver.quit();
+
     }
 
+    @Then("^the email is in the sent folder with ([^\"]*) recipient, and subject ([^\"@]*)$")
+    public void checkSentFolder(String email, String subject) throws Throwable {
+        goToSent();
+        System.out.println("In Sent");
+        email = email.substring(0,email.indexOf('@'));
+        String ePath  = "//span[contains(@name,'"+email+"')]";
+        String sPath  = "//span[contains(text(),'"+subject+"')]";
+        Assert.assertTrue(foundByXPath(ePath) && foundByXPath(sPath));
+        System.out.println("DONE!");
+        //driver.quit();
+    }
+
+    @Then("^the email is in the sent folder with only ([^\"]*) recipient$")
+    public void checkSentFolder(String email) throws Throwable {
+        goToSent();
+        email = email.substring(0,email.indexOf('@'));
+        String ePath  = "//span[contains(@name,'"+email+"')]";
+        Assert.assertTrue(foundByXPath(ePath));
+        System.out.println("DONE!");
+        //driver.quit();
+    }
 
 
     @Then("^the system shall warn me that there is no subject nor body$")
@@ -311,7 +306,7 @@ public class StepDefinitions {
 
 
 
-    @When("^I click the ‘Ok’ button$")
+    @When("^I acknowledge the warning$")
     public void iPressOk() throws Throwable {
         System.out.println("Attempting to find alert... ");
         if(alert != null){
@@ -361,8 +356,55 @@ public class StepDefinitions {
         //I.e. assert that the confirmation message does not show up, thus the email was not sent
         Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(),'Message sent.')]")).isEmpty());
         //Thread.sleep(5000); //For user visibility purposes during execution
+       // driver.quit();
+    }
+
+    @After //TODO
+    public void tearDownTests(Scenario scenario) throws InterruptedException {
+        //I.e. if failed to login to sent tab
+        //driver = null; //reset driver
+/*        if(!setupTearDownHelper()){
+            System.out.println("Failed to setup @After");
+            driver.quit();
+            Assert.fail();
+        }*/
+        //No email sent thus nothing to clean
+        if(scenario.getName().equals("Attaching an image and sending to an invalid recipient")){
+            driver.quit();
+            return;
+        }
+        System.out.println("TearDown");
+        goToSent();
+        //DELETE any sent emails
+        if(!driver.findElements(By.xpath("//div[contains(@aria-label,'Show more messages')]")).isEmpty()){
+            //Click select all button
+            System.out.println("Finding select button");
+
+            //List<WebElement> boxes = driver.findElements(By.xpath("//div[contains(@class,'T-Jo-auh')]"));
+            //boxes.get(1).click();
+            List<WebElement> selector = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[contains(@class,'T-Jo-auh')]")));
+            System.out.println("Selector size:\t"+selector.size());
+            WebElement parent = selector.get(1).findElement(By.xpath(".."));
+            System.out.println(parent.getAttribute("innerHTML"));
+            parent = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(parent));
+            System.out.println("click");
+            parent.click();
+            //.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'G-asx T-I-J3 J-J5-Ji')]")));
+            //driver.findElements(By.xpath("//span[contains(@role,'checkbox')]")).get(0);
+            //.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(@class,'T-Jo J-J5-Ji')]")));
+
+            //selector.click();
+            //driver.findElement(By.xpath("//li[@class='select2-search-field']//input[@class='select2-input' and contains(@id,'id_autogen')]")).click();
+
+            //Click delete button
+            System.out.println("Finding delete button");
+            WebElement delete = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@aria-label,'Delete')]")));
+            delete.click();
+            Thread.sleep(1000);
+        }
         driver.quit();
     }
+
     //----------------------------------------OLD--------------------------------------------
 
     /**
@@ -373,34 +415,47 @@ public class StepDefinitions {
             System.out.println("Setting up ChromeDriver... ");
             System.setProperty("webdriver.chrome.driver", PATH_TO_CHROME_DRIVER);
             driver = new ChromeDriver();
+            waiter = new WebDriverWait(driver, 10);
             System.out.print("Done!\n");
         }
     }
 
-    private void enterText(WebElement textField,String str,Keys fin){
+    private void enterText(WebElement textField,String str,Keys fin) throws InterruptedException {
         //textField.sendKeys(Keys.TAB);
         textField.clear();
         textField.sendKeys(str);
+        Thread.sleep(300);
         textField.sendKeys(fin);
     }
 
-    private WebElement findElement(String type){
-        WebElement el=null;
-
-        if(type.equals("body")){
-            System.out.println("Attempting to find "+type+" Field... ");
-            el = (new WebDriverWait(driver, 10))
+    public void iPressAttach() throws Throwable {
+        try {
+            System.out.println("Attempting to find Attach button... ");
+            WebElement btn = (new WebDriverWait(driver, 10))
                     .until(ExpectedConditions.elementToBeClickable(
-                            By.xpath("//div[contains(@aria-label,'Message Body')]"))); //NOTE assumes english
+                            By.xpath("//div[contains(@class,'a1 aaA aMZ')]"))); //Note: assumes class name doesn't change
             System.out.print("Found!\n");
-        } else { //subject
-            System.out.println("Attempting to find subject Field");
-            el = (new WebDriverWait(driver, 10))
-                    .until(ExpectedConditions.elementToBeClickable(
-                            By.xpath("//input[contains(@aria-label,'Subject')]"))); //NOTE assumes english
-            System.out.print("Found!\n");
+            btn.click();
+            Thread.sleep(1000); //TODO: deal with timing
+            System.out.println("Clicking Attach button.");
+        } catch (Exception e) {
+            System.out.println("No Attach button found");
         }
-        return el;
+    }
+
+    private boolean foundByXPath(String path) throws Throwable{
+        try{
+            System.out.println("Finding: \t"+path);
+            WebElement textField = (new WebDriverWait(driver, 10))
+                    .until(ExpectedConditions.presenceOfElementLocated(
+                            By.xpath(path)));
+            System.out.println("Found");
+            return true;
+        }catch (Exception e){
+            System.out.println("Not Found by Xpath: \t"+path);
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     private boolean searchForText(String text, String textToFind) { //TODO: is this necessary?
@@ -415,7 +470,7 @@ public class StepDefinitions {
     }
 
     //This helper goes to the sent tab
-    private boolean setupTearDownHelper(){
+    private boolean setUpHelper(){
         try{
             givenLoggedIn();
         } catch (Throwable throwable) {
@@ -423,15 +478,17 @@ public class StepDefinitions {
             throwable.printStackTrace();
             return false;
         }
+        return true;
+    }
+
+    private void goToSent(){
         //SWITCH TO SENT TAB
         System.out.println("Attempting to find Send tab... ");
-        WebElement tab = (new WebDriverWait(driver, 10))
+        WebElement tab = (new WebDriverWait(driver, 15))
                 .until(ExpectedConditions.elementToBeClickable(
                         By.linkText("Sent"))); //TODO assumes english
         System.out.print("Found!\n");
         System.out.print("Clicking!\n");
         tab.click();
-        return true;
     }
-
 }
--- src/test/resources/ecse428a2/gmail.feature
+++ src/test/resources/ecse428a2/gmail.feature
@@ -2,84 +2,85 @@ Feature: Gmail
 
   Scenario Outline: Attaching to an email (with text) image(s) and sending to recipient
     Given I am logged in
-    When I click the ‘Compose’ button
-    And I enter a valid <valid email> in the ‘to’ section
-    And I enter <text> in <section> section
-    And I click the ‘Attach files’ button
+    When I compose an email
+    And I enter a valid <valid email> recipient
+    And I enter subject <text>
     When I select the file <file> I want to send
-    And I click the ‘Send’ button
+    And I send the email
     Then the email shall be sent
+    And the email is in the sent folder with <valid email> recipient, and subject <text>
 
     Examples:
     #Values for body/Subject: body, subject, both
     #Values for file: small, medium, large, small \n medium
-      | valid email                    | file      | text | section|
-      | GabrielNegashECSE428@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | One | body |
-      | GTNECSE4281@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | Two | body |
-      | GTNECSE4282@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg | Three| subject |
-      | GTNECSE4283@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg | Four | both |
-      | GTNECSE4284@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg| Five | subject |
+      | valid email                    | file      | text |
+      | GTNECSE4281@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | One |
+      | GTNECSE4282@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | Two |
+      | GTNECSE4283@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg | Three|
+      | GTNECSE4284@yahoo.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | Four  |
+      | GTNECSE4285@yahoo.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg| Five |
 
+  #Sending image(s) without a subject and body
   Scenario Outline: Sending image(s) without a subject and body
     Given I am logged in
-    When I click the ‘Compose’ button
-    And I enter a valid <valid email> in the ‘to’ section
-    And I click the ‘Attach files’ button
+    When I compose an email
+    And I enter a valid <valid email> recipient
     When I select the file <file> I want to send
-    And I click the ‘Send’ button
+    And I send the email
     Then the system shall warn me that there is no subject nor body
-    When I click the ‘Ok’ button
+    When I acknowledge the warning
     Then the email shall be sent
+    And the email is in the sent folder with only <valid email> recipient
+
     Examples:
     #Values for file: small, medium, large, small \n medium
       | valid email                    | file      |
-      | GabrielNegashECSE428@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg |
       | GTNECSE4281@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg   |
       | GTNECSE4282@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg  |
       | GTNECSE4283@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg   |
-      | GTNECSE4284@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg|
+      | GTNECSE4284@yahoo.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg|
+      | GTNECSE4285@yahoo.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg |
 
 
   Scenario Outline: Attaching an image that is too large
     Given I am logged in
-    When I click the ‘Compose’ button
-    And I enter a valid <valid email> in the ‘to’ section
-    And I enter <text> in <section> section
-    And I click the ‘Attach files’ button
+    When I compose an email
+    And I enter a valid <valid email> recipient
+    And I enter subject <text>
     When I select the file <file> I want to send
     But the cumulative size of the files exceed the attachment limit
     Then the system shall attach the file(s) <file> as google drive link(s)
-    When I click the ‘Send’ button
+    When I send the email
     And I confirm drive permissions for <valid email>
     Then the email shall be sent
+    And the email is in the sent folder with <valid email> recipient, and subject <text>
 
     Examples:
     #Values for body/Subject: body, subject, both
     #Values for file: xl2, xl1 \n large2, small \n xl2, large \n large2, small \n medium \n large \xl1
-      | valid email                    | file      | text | section|
-      | GabrielNegashECSE428@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\xl2.jpg | One | body |
-      | GTNECSE4281@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\xl1.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\large2.jpg | Two | body |
-      | GTNECSE4282@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\xl2.jpg | Three| subject |
-      | GTNECSE4283@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\large2.jpg | Four | both |
-      | GTNECSE4284@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\xl1.jpg | Five | subject |
+      | valid email                    | file      | text |
+      | GTNECSE4281@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\xl2.jpg | One |
+      | GTNECSE4282@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\xl1.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\large2.jpg | Two |
+      | GTNECSE4283@gmail.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\xl2.jpg | Three|
+      | GTNECSE4284@yahoo.com | C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\large2.jpg | Four |
+      | GTNECSE4285@yahoo.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\xl1.jpg | Five |
 
   Scenario Outline: Attaching an image and sending to an invalid recipient
     Given I am logged in
-    When I click the ‘Compose’ button
-    And I input an invalid <invalid email> in the ‘to’ section
-    And I enter <text> in <section> section
-    And I click the ‘Attach files’ button
+    When I compose an email
+    And I input an invalid <invalid email> recipient
+    And I enter subject <text>
     When I select the file <file> I want to send
-    And I click the ‘Send’ button
+    And I send the email
     Then an error message shall be returned
     And the email shall not be sent
 
     Examples:
     #Values for body/Subject: body, subject, both
     #Values for file: small, medium, large, small \n medium
-      | invalid email                    | file      | text | section|
-      | GabrielNegashECSE428.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | One | body |
-      | GTNECSE4281.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | Two | body |
-      | GTNECSE4282.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg | Three| subject |
-      | GTNECSE4283.com | C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg | Four | both |
-      | GTNECSE4284.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg| Five | subject |
+      | invalid email                    | file      | text |
+      | GTNECSE4281.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | One |
+      | GTNECSE4282.com | C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg | Two |
+      | GTNECSE4283.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg | Three|
+      | GTNECSE4284.com | C:\Users\Gabriel\Documents\ecse428a2\pics\large.jpg | Four |
+      | GTNECSE4285.com | C:\Users\Gabriel\Documents\ecse428a2\pics\medium.jpg \n C:\Users\Gabriel\Documents\ecse428a2\pics\small.jpg| Five |
